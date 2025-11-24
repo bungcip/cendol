daf32e068431712726b7faa7d26670a6cd3ecbd4
bungcip <bungcip@gmail.com>
2025-11-24 22:34:05 +0700

    add test to pplexer

--- src/preprocessor/tests_pp_lexer.rs
+++ src/preprocessor/tests_pp_lexer.rs
@@ -8,6 +8,19 @@ fn create_test_pp_lexer(source: &str) -> PPLexer {
     PPLexer::new(source_id, buffer)
 }
 
+/// Macro to test multiple token productions in sequence
+macro_rules! test_tokens {
+    ($lexer:expr, $( ($input:expr, $expected:pat) ),* $(,)?) => {
+        $(
+            let token = $lexer.next_token().unwrap();
+            match token.kind {
+                $expected => {},
+                _ => panic!("Expected {:?}, got {:?}", stringify!($expected), token.kind),
+            }
+        )*
+    };
+}
+
 /// Test line splicing: backslash followed by newline removes both characters
 #[test]
 fn test_line_splicing_basic() {
@@ -174,4 +187,144 @@ world\"";
         }
         _ => panic!("Expected string literal token"),
     }
+}
+
+/// Test that PPLexer can produce all punctuation tokens
+#[test]
+fn test_all_punctuation_tokens() {
+    let source = "+ - * / % & | ^ ! ~ < > <= >= == != << >> = += -= *= /= %= &= |= ^= <<= >>= ++ -- -> . ? : , ; ( ) [ ] { } ... && || # ##";
+    let mut lexer = create_test_pp_lexer(source);
+
+    test_tokens!(lexer,
+        ( "+", PPTokenKind::Plus ),
+        ( "-", PPTokenKind::Minus ),
+        ( "*", PPTokenKind::Star ),
+        ( "/", PPTokenKind::Slash ),
+        ( "%", PPTokenKind::Percent ),
+        ( "&", PPTokenKind::And ),
+        ( "|", PPTokenKind::Or ),
+        ( "^", PPTokenKind::Xor ),
+        ( "!", PPTokenKind::Not ),
+        ( "~", PPTokenKind::Tilde ),
+        ( "<", PPTokenKind::Less ),
+        ( ">", PPTokenKind::Greater ),
+        ( "<=", PPTokenKind::LessEqual ),
+        ( ">=", PPTokenKind::GreaterEqual ),
+        ( "==", PPTokenKind::Equal ),
+        ( "!=", PPTokenKind::NotEqual ),
+        ( "<<", PPTokenKind::LeftShift ),
+        ( ">>", PPTokenKind::RightShift ),
+        ( "=", PPTokenKind::Assign ),
+        ( "+=", PPTokenKind::PlusAssign ),
+        ( "-=", PPTokenKind::MinusAssign ),
+        ( "*=", PPTokenKind::StarAssign ),
+        ( "/=", PPTokenKind::DivAssign ),
+        ( "%=", PPTokenKind::ModAssign ),
+        ( "&=", PPTokenKind::AndAssign ),
+        ( "|=", PPTokenKind::OrAssign ),
+        ( "^=", PPTokenKind::XorAssign ),
+        ( "<<=", PPTokenKind::LeftShiftAssign ),
+        ( ">>=", PPTokenKind::RightShiftAssign ),
+        ( "++", PPTokenKind::Increment ),
+        ( "--", PPTokenKind::Decrement ),
+        ( "->", PPTokenKind::Arrow ),
+        ( ".", PPTokenKind::Dot ),
+        ( "?", PPTokenKind::Question ),
+        ( ":", PPTokenKind::Colon ),
+        ( ",", PPTokenKind::Comma ),
+        ( ";", PPTokenKind::Semicolon ),
+        ( "(", PPTokenKind::LeftParen ),
+        ( ")", PPTokenKind::RightParen ),
+        ( "[", PPTokenKind::LeftBracket ),
+        ( "]", PPTokenKind::RightBracket ),
+        ( "{", PPTokenKind::LeftBrace ),
+        ( "}", PPTokenKind::RightBrace ),
+        ( "...", PPTokenKind::Ellipsis ),
+        ( "&&", PPTokenKind::LogicAnd ),
+        ( "||", PPTokenKind::LogicOr ),
+        ( "#", PPTokenKind::Hash ),
+        ( "##", PPTokenKind::HashHash ),
+    );
+}
+
+/// Test that PPLexer can produce all keyword tokens
+#[test]
+fn test_all_keyword_tokens() {
+    let source = "if ifdef ifndef elif else endif define undef include line pragma error warning";
+    let mut lexer = create_test_pp_lexer(source);
+
+    test_tokens!(lexer,
+        ( "if", PPTokenKind::If ),
+        ( "ifdef", PPTokenKind::Ifdef ),
+        ( "ifndef", PPTokenKind::Ifndef ),
+        ( "elif", PPTokenKind::Elif ),
+        ( "else", PPTokenKind::Else ),
+        ( "endif", PPTokenKind::Endif ),
+        ( "define", PPTokenKind::Define ),
+        ( "undef", PPTokenKind::Undef ),
+        ( "include", PPTokenKind::Include ),
+        ( "line", PPTokenKind::Line ),
+        ( "pragma", PPTokenKind::Pragma ),
+        ( "error", PPTokenKind::Error ),
+        ( "warning", PPTokenKind::Warning ),
+    );
+}
+
+/// Test that PPLexer can produce all literal tokens
+#[test]
+fn test_all_literal_tokens() {
+    let source = "variable \"string\" 'c' 123";
+    let mut lexer = create_test_pp_lexer(source);
+
+    let token1 = lexer.next_token().unwrap();
+    match token1.kind {
+        PPTokenKind::Identifier(_) => {},
+        _ => panic!("Expected identifier token"),
+    }
+
+    let token2 = lexer.next_token().unwrap();
+    match token2.kind {
+        PPTokenKind::StringLiteral(_) => {},
+        _ => panic!("Expected string literal token"),
+    }
+
+    let token3 = lexer.next_token().unwrap();
+    match token3.kind {
+        PPTokenKind::CharLiteral(_) => {},
+        _ => panic!("Expected char literal token"),
+    }
+
+    let token4 = lexer.next_token().unwrap();
+    match token4.kind {
+        PPTokenKind::Number(_) => {},
+        _ => panic!("Expected number token"),
+    }
+}
+
+/// Test that adjacent string literals are not combined in preprocessor stage
+#[test]
+fn test_adjacent_string_literals_not_combined() {
+    let source = "\"hello\" \"world\"";
+    let mut lexer = create_test_pp_lexer(source);
+
+    // First string literal
+    let token1 = lexer.next_token().unwrap();
+    match token1.kind {
+        PPTokenKind::StringLiteral(symbol) => {
+            assert_eq!(symbol.as_str(), "\"hello\"");
+        }
+        _ => panic!("Expected first string literal token"),
+    }
+
+    // Second string literal (should be separate, not combined)
+    let token2 = lexer.next_token().unwrap();
+    match token2.kind {
+        PPTokenKind::StringLiteral(symbol) => {
+            assert_eq!(symbol.as_str(), "\"world\"");
+        }
+        _ => panic!("Expected second string literal token"),
+    }
+
+    // Should be no more tokens
+    assert!(lexer.next_token().is_none());
 }

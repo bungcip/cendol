87672f32316d369040b7bccafcc7572e9fe42019
bungcip <bungcip@gmail.com>
2025-11-30 11:31:09 +0700

    update cargo.toml

--- src/parser/tests_parser.rs
+++ src/parser/tests_parser.rs
@@ -835,12 +835,12 @@ fn test_function_with_array_abstract_declarator() {
 #[test]
 fn test_complex_abstract_declarator_function() {
     let resolved = parse_declaration("int f5(int (*fp)(int));");
-    insta::assert_yaml_snapshot!(&resolved, @r#"
+    insta::assert_yaml_snapshot!(&resolved, @r"
     Declaration:
       specifiers:
         - int
       init_declarators:
         - name: f5
           kind: function(int function(int) -> pointer) -> int
-    "#);
+    ");
 }
